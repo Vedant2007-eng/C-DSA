9f5b5ba92e9d45883b2065fd960a6a0019ec8439
Vedant2007-eng <vedantkubde748@gmail.com>
2026-05-28 12:41:18 +0530

    Create show the example of AND bitwise operator.cpp

---
+++ Day3/show the example of AND bitwise operator.cpp
@@ -0,0 +1,9 @@
+//Question --> show the example of AND bitwise operator
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 4;
+    int b = 8;
+    cout << (a&b) << endl;
+    return 0;
+}
