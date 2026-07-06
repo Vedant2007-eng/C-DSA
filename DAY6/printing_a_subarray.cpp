0f9edd0ac25e2ed8ef016cb8a28ca3813bb3248b
Vedant2007-eng <vedantkubde748@gmail.com>
2026-07-06 08:46:30 +0530

    Add subarray printing functionality
    Implement a program to print all subarrays of a fixed array.

---
+++ DAY6/printing_a_subarray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int st;
+    int end;
+    int n = 5;
+    int arr[5] = {1,2,3,4,5};
+    for(st = 0; st < n; st++){
+        for(end = 0; end <= n-1; end++){
+            for(int i = st; i < end; i++){
+                cout << arr[i];
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
