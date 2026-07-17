1f382b43bcfc98ae4b7c9425b9659f7a72d97fd4
Vedant2007-eng <vedantkubde748@gmail.com>
2026-07-17 19:44:30 +0530

    Add double pointer example in using_dereferencing_operator.cpp

--- Day11/using_derefrencing_operator.cpp
+++ Day11/using_derefrencing_operator.cpp
@@ -3,9 +3,11 @@ using namespace std;
 int main(){
     int num = 1;
     int* ptr = &num;
+    int** parptr = &ptr;
     
     cout << *(&num) << endl;
     cout << *(ptr) << endl;
+    cout << *(*(parptr));
 
     return 0;
 }
