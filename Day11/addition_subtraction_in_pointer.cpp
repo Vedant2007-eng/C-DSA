9763eea755fb75af0e5306b65a0a1ed0344481fa
Vedant2007-eng <vedantkubde748@gmail.com>
2026-07-18 12:07:03 +0530

    Create addition_subtraction_in_pointer.cpp

---
+++ Day11/addition_subtraction_in_pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int ptr[] = {1,2,4,5,6};
+
+    cout << ptr << endl;
+    cout << *ptr << endl;
+    cout << *ptr + 1 << endl;
+    cout << *ptr + 2 << endl;
+    cout << *ptr + 3 << endl;
+    cout << *ptr + 4 << endl;
+
+    return 0;
+}
